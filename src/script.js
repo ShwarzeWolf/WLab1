08174b651ef161b60ca9cd3c1ffdf2858df96239
Kristin <1358365@DESKTOP-4FH9G41>
2019-10-28 13:50:03 +0300

    getting data from site added

--- src/script.js
+++ src/script.js
@@ -1,4 +1,9 @@
 function searchCity(){
     let city= document.getElementById("city").value;
-    alert(city)
+
+    (async () => {
+        let response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=f51bcfb8b207b0ef58ce10da80b90477");
+        let data = await response.text();
+        alert(data);
+    })()
 }
