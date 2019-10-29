19692f9cbd964b9aa95d20da675876fd5a491fa0
Kristin <1358365@DESKTOP-4FH9G41>
2019-10-29 22:39:56 +0300

    templates added

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
-function searchCity(){
-    let city=document.getElementById("cityName").value;
+function searchCity() {
+    let content = document.getElementById("content");
+    let city = document.getElementById("cityName").value;
 
-    (async () => {
-        let response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
-            city +
-            "&appid=f51bcfb8b207b0ef58ce10da80b90477");
+    let url = "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&appid=f51bcfb8b207b0ef58ce10da80b90477";
 
-        if (response.ok){
+    fetch(url)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json()
+            }
+        })
+        .then(function (json) {
+            drawData(content, json);
+        });
 
-        }
-        let data = await response.json();
-
-        let dataToDraw = new Map;
-
-        dataToDraw.set("generalDescription", data['weather'][0]['description']);
-        dataToDraw.set("temperature", data['main']['temp']);
-        dataToDraw.set("pressure", data['main']['pressure']);
-        dataToDraw.set("humidity", data['main']['humidity']);
-        dataToDraw.set("windSpeed", data['wind']['speed']);
-        dataToDraw.set("clouds", data['clouds']['all']);
-    }
-    )()
 }
 
-function drawData()
+function drawData(content, json){
+
+    let template = '<div class="name" id="name">{{name}}</div>\n' +
+        '    <div class="temp" id="temp">{{main.temp}}°C</div>\n' +
+        '    <div class="weather" id="weather">{{weather.0.main}}</div>\n' +
+        '    <div class="wind" id="wind">Wind:</br> {{wind.speed}} meter/sec</div>\n' +
+        '    <div class="humidity" id="humidity">Humidity:</br> {{main.humidity}}%</div>';
+
+    if (json) {
+        content.innerHTML += Mustache.render(template, json);
+    }
+}
+
